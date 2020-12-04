40108e328356180d41fa152104dc2a855361c57d
Ash Caplan <accaplan93@gmail.com>
2020-12-04 18:12:40 +0100

    git ignore changes

---
+++ web/src/components/disclaimer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Disclaimer = () => {
+  return (
+    <div className='bg--blue x color--white'>
+      <div className='container--1000 mxa p2 tc outer'>
+        <div>
+          <p>Coming January 2020!</p>
+        </div>
+      </div>
+    </div>
+  )
+}
