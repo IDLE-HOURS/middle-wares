406e20c9a787d31b5be89b5ac844128f2e3c9eef
Ash Caplan <accaplan93@gmail.com>
2020-12-05 11:05:30 +0100

    watchmode error

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -80,7 +80,7 @@ module.exports = {
         projectId: process.env.SANITY_PROJECT_ID,
         dataset: process.env.SANITY_DATASET,
         token: process.env.SANITY_API_TOKEN,
-        watchMode: true
+        watchMode: false
       }
     },
     {
